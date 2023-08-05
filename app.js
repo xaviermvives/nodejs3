f6b8b3ffd1331d41522734534c1bf605e4e33050
xavier <montjuic79@gmail.com>
2023-08-05 20:19:45 +0200

    GET movies route

---
+++ app.js
@@ -0,0 +1,24 @@
+/* eslint-disable indent */
+const express = require('express')
+const movies = require('./movies.json')
+
+const app = express()
+
+app.disable('x-powered-by')
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({ message: 'hola primo!' })
+    // res.end('Hola world!')
+})
+
+app.get('/movies', (req, res) => {
+    res.json(movies)
+})
+
+const PORT = process.env.PORT ?? 3005
+
+app.listen(PORT, () => {
+    console.log(`server listening on port http://localhost:${PORT}`)
+})

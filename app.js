cc95051ff35e7a78d63dcafa7e6c46290761312b
xavier <montjuic79@gmail.com>
2023-08-07 12:30:05 +0200

    POST without validation

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 /* eslint-disable indent */
 const express = require('express')
 const movies = require('./movies.json')
+const crypto = require('node:crypto')
 
 const app = express()
 
 app.disable('x-powered-by')
 
-// app.use(express.json())
+app.use(express.json())
 
 app.get('/movies', (req, res) => {
     const { genre } = req.query
@@ -26,6 +27,32 @@ app.get('/movies/:id', (req, res) => { // path-to-regexp
     if (movie) return res.json(movie)
 
     res.status(404).json({ message: 'Movie not found' })
+})
+
+app.post('/movies', (req, res) => {
+    const {
+        title,
+        genre,
+        director, year,
+        duration, poster,
+        rate
+    } = req.body
+
+    const newMovie = {
+        id: crypto.randomUUID(), // uuid v4
+        title,
+        genre,
+        director,
+        year,
+        duration,
+        poster,
+        rate: rate ?? 0
+    }
+
+    // Esto no seria REST pq estaríamos guardando el estado de la aplicación en memoria
+    movies.push(newMovie)
+
+    res.status(201).json(newMovie) // reenviamos para actualizar la caché del cliente
 })
 
 const PORT = process.env.PORT ?? 3005
